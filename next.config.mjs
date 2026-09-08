2775cf15c0025f6b3d763523f29f969f6ccf8bb9
Premlal <premlalbanavath@gmail.com>
2026-09-09 01:00:51 +0530

    fix: configure conditional basePath for GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   output: 'export',
+  basePath: isGithubActions ? '/puffy-3d-ecom' : '',
+  assetPrefix: isGithubActions ? '/puffy-3d-ecom' : '',
   images: {
     unoptimized: true,
   },
